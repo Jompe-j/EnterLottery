89ad1d61f0fc97b02eb2a2983a08bea1e5ddc51c
Pompe <pompepuma@gmail.com>
2020-08-28 17:05:48 +0200

    get numbers

---
+++ EnterLottery/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EnterLottery {
+    internal class Program {
+        public static void Main(string[] args) {
+            var numbers = GetNumbers();
+        
+        }
+        private static Array GetNumbers() {
+            var numbers = new int[36];
+            for (int i = 0; i < numbers.Length; i++) {
+                numbers[i] = i + 1;
+            }
+
+            return numbers;
+        }
+    }
+}
